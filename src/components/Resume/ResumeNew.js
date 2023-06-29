11cc1320d1477e0d2a9b2e0a48a70b1c8cbefa0f
Solomon Yeshiwas <solomonyeshiwas532@gmail.com>
2023-06-29 11:39:50 +0300

    resume link

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -8,7 +8,7 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink ="https://github.com/solomonyeshiwas/port/blob/main/src/Assets/solomoncv2023.pdf";
+const resumeLink ="https://raw.githubusercontent.com/solomonyeshiwas/port/main/src/Assets/solomoncv2023.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
